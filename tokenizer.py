bdc37f1dab63689cf6a366f2136c269a45536caa
nightmono <163682003+nightmono@users.noreply.github.com>
2024-06-11 12:30:26 +0100

    Put all handlers into functions

--- tokenizer.py
+++ tokenizer.py
@@ -1,3 +1,32 @@
+def handle_whitespace(stream, head):
+    while head < len(stream) and stream[head] == " ":
+        head += 1
+    return head
+
+def handle_number(stream, tokens, current, head):
+    while head < len(stream) and stream[head] in ".0123456789":
+        head += 1
+    tokens.append(stream[current:head])
+    return head
+
+def handle_one_char(stream, tokens, current):
+    tokens.append(stream[current])
+    return current + 1
+
+def handle_string(stream, tokens, current, head):
+    while head < len(stream) and stream[head] not in "'\"":
+        head += 1
+    tokens.append(stream[current+1:head])
+    # Avoid handing the ending quote/speech mark
+    return head + 1
+
+def handle_func(stream, tokens, funcs, current, head):
+    # Pick the function with the lowest index.
+    func = min(funcs, key=lambda x: stream.index(x))
+    head += len(func)
+    tokens.append(stream[current:head])
+    return head
+
 def tokenize(stream: str, functions_list: list[str] = None):
     stream = stream.strip()
     functions_list = functions_list or []
@@ -11,36 +40,23 @@ def tokenize(stream: str, functions_list: list[str] = None):
     while current < len(stream):
         # Whitespace.
         if stream[current] == " ":
-            while head < len(stream) and stream[head] == " ":
-                head += 1
+            head = handle_whitespace(stream, head+1)
 
         # Numbers.
         elif stream[current] in ".0123456789":
-            while head < len(stream) and stream[head] in ".0123456789":
-                head += 1
-            tokens.append(stream[current:head])
+            head = handle_number(stream, tokens, current, head+1)
 
         # One character tokens.
         elif stream[current] in "+-*/(),":
-            head += 1
-            tokens.append(stream[current])
+            head = handle_one_char(stream, tokens, current)
 
         # Strings.
         elif stream[current] in "'\"":
-            # Avoid handling the opening quote/speech mark
-            head += 1
-            while head < len(stream) and stream[head] not in "'\"":
-                head += 1
-            tokens.append(stream[current+1:head])
-            # Avoid handing the ending quote/speech mark
-            head += 1
+            head = handle_string(stream, tokens, current, head+1)
 
         # Functions.
         elif funcs := ([func for func in functions_list if func in stream[current:]]):
-            # Pick the function with the lowest index.
-            func = min(funcs, key=lambda x: stream.index(x))
-            head += len(func)
-            tokens.append(stream[current:head])
+            head = handle_func(stream, tokens, funcs, current, head)
 
         # Unrecognised character error handling.
         else:
